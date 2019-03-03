45d83a3154e673dbdc133b687b051cb060abf72a
Douglas Prado <douglasrafaeldoprado@gmail.com>
2019-03-03 14:48:16 -0300

    create button nubank

---
+++ src/screens/Main/style.js
@@ -0,0 +1,14 @@
+import React from "react";
+import styled from "styled-components/native";
+
+export const Container = styled.View`
+  flex: 1;
+  background-color: rgb(255, 33, 119);
+`;
+export const SwiperContainer = styled.View`
+  flex: 1;
+  margin-left: 20;
+  margin-right: 20;
+  align-items: center;
+  justify-content: center;
+`;
